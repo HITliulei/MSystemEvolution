dfb8f9e385997fd3e02ca6fcd4108c1076e5cdd6
liulei <liulei13041x@163.com>
2019-12-28 17:08:40 +0800

    功能描述注解

---
+++ MClientFramework/src/main/java/com/septemberhx/mclient/annotation/MFuncDescription.java
@@ -0,0 +1,15 @@
+package com.septemberhx.mclient.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by Lei on 2019/12/28 17:07
+ */
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MFuncDescription {
+    String value() default "";
+    int lavel() default 1;
+}
