e83390d14121e8d2659bb450366e3687ddb1c755
SeptemberHX <september_hx@outlook.com>
2020-04-16 00:08:27 +0800

    fix: bugs in cloud call

--- MOrchestrationServer/src/main/java/com/septemberhx/server/controller/MSvcDepController.java
+++ MOrchestrationServer/src/main/java/com/septemberhx/server/controller/MSvcDepController.java
@@ -11,6 +11,7 @@ import com.septemberhx.common.service.MSvcInstance;
 import com.septemberhx.common.utils.MRequestUtils;
 import com.septemberhx.common.utils.MUrlUtils;
 import com.septemberhx.server.model.MServerSkeleton;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +25,7 @@ import java.util.*;
  * @version 0.1
  * @date 2020/3/7
  */
+@Controller
 public class MSvcDepController {
 
     @PostMapping(path = MConfig.MSERVER_NEW_DEP_REQUEST)
